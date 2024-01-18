34f65a9e1efc507a7b4e9ad840b6ee9797af8702
Gaurav Rawat <gauravrawat@Gauravs-MacBook-Air.local>
2024-01-18 15:48:53 -0500

    fragment added

--- app/src/main/java/com/example/recordkeeper/CyclingFragment.kt
+++ app/src/main/java/com/example/recordkeeper/CyclingFragment.kt
@@ -6,13 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class RunningFragment:Fragment() {
+class CyclingFragment:Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_running, container, false)
+        return inflater.inflate(R.layout.fragment_cycling, container, false)
     }
 }
--- app/src/main/java/com/example/recordkeeper/MainActivity.kt
+++ app/src/main/java/com/example/recordkeeper/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.recordkeeper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import androidx.fragment.app.commit
 import com.example.recordkeeper.databinding.ActivityMainBinding
 
@@ -14,10 +15,30 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
 
+        binding.bottomNav.setOnItemReselectedListener {
+            if(it.itemId == R.id.nav_cycling){
+                onCyclingClicked()
+            }
+            else if(it.itemId == R.id.nav_running){
+                onRunningClicked()
+            }
+        }
+
+
+
+    }
+
+    fun onRunningClicked(){
         //handles fragments
         supportFragmentManager.commit {
             replace(R.id.frame_content, RunningFragment())
         }
+    }
 
+    fun onCyclingClicked(){
+        //handles fragments
+        supportFragmentManager.commit {
+            replace(R.id.frame_content, CyclingFragment())
+        }
     }
 }
